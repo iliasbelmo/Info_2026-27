2fea33c734c86aa2d1f84527654abd71e0a4090a
73867139 <iliasbelmoqaddem@gmail.com>
2026-09-19 09:25:38 +0200

    aggiornamento

--- progetto-lampadina/src/Main.java
+++ progetto-lampadina/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-    Lampadina l;
-    l = new Lampadina();
-    l.potenza = 30;
+        Lampadina a, b;
+        a = new Lampadina();
+        b = new Lampadina();
+        a.potenza = 50;
+        b.potenza = 10;
     }
 }
