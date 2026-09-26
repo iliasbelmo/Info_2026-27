aaec82d1076b93ac07ec477c98a8bacad8c9bfcf
73867139 <iliasbelmoqaddem@gmail.com>
2026-09-26 08:55:06 +0200

    cambiamento

--- progetto-studenti/src/Studenti.java
+++ progetto-studenti/src/Studenti.java
@@ -1,6 +1,12 @@
 public class Studenti {
-    String nome;
-    String cognome;
-    float peso;
-    float altezza;
+    private String nome;
+    private String cognome;
+    private int eta;
+    private double peso;
+    private double altezza;
+
+    public Studenti (String name, String surname, int age, float height){
+
+
+    }
 }
