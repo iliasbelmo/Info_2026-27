9a2cc17b0b45dd3c13014f4cd7e89226d9c496b1
73867139 <iliasbelmoqaddem@gmail.com>
2026-09-26 08:59:09 +0200

    cambiamento

--- progetto-studenti/src/Studenti.java
+++ progetto-studenti/src/Studenti.java
@@ -5,11 +5,35 @@ public class Studenti {
     private double peso;
     private double altezza;
 
-    public Studente() {
+    public Studenti () {
         this.nome = "";
         this.cognome = "";
         this.eta = 0;
         this.altezza = 0.0;
         this.peso = 0.0;
     }
+    public Studenti (Studenti  altro) {
+        this.nome = altro.nome;
+        this.cognome = altro.cognome;
+
+        if (altro.eta > 0) {
+            this.eta = altro.eta;
+        } else {
+            this.eta = 0;
+        }
+        if (altro.altezza > 0) {
+            this.altezza = altro.altezza;
+        } else {
+            this.altezza = 0.0;
+        }
+
+        if (altro.peso > 0) {
+            this.peso = altro.peso;
+        } else {
+            this.peso = 0.0;
+        }
+    }
+
+
+
 }
